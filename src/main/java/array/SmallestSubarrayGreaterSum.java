7862451bab37c79d87d71eed155903a4403e17f4
Vương <vuong.le@onemount.com>
2022-09-11 15:08:59 +0700

    feat(array): SmallestSubarrayGreaterSum

--- src/main/java/array/SmallestSubarrayGreaterSum.java
+++ src/main/java/array/SmallestSubarrayGreaterSum.java
@@ -6,8 +6,9 @@ public class SmallestSubarrayGreaterSum {
         int windowSum = 0;
         int windowSize = 0;
         int minWindowSize = 99999;
+        int windowStart = 0;
 
-        for (int i = 0; i < a.length - 1; i++) {
+        for (int i = 0; i < a.length; i++) {
             windowSum += a[i];
             windowSize++;
 
@@ -16,7 +17,18 @@ public class SmallestSubarrayGreaterSum {
                     minWindowSize = windowSize;
                 }
 
+                while (windowSum >= s) {
+                    windowSum -= a[windowStart];
+                    windowStart++;
+                    windowSize--;
+                    if (windowSum < s) {
+                        continue;
+                    }
 
+                    if (minWindowSize > windowSize) {
+                        minWindowSize = windowSize;
+                    }
+                }
             }
         }
 
@@ -24,7 +36,9 @@ public class SmallestSubarrayGreaterSum {
     }
 
     public static void main(String[] args) {
-        int[] a = {2, 1, 5, 2, 3, 2};
-        System.out.println(findMinSubArray(a, 7));
+//        int[] a = {2, 1, 5, 2, 3, 2};
+//        int[] a = {2, 1, 5, 2, 8};
+        int[] a = {3, 4, 1, 1, 6};
+        System.out.println(findMinSubArray(a, 8));
     }
 }
